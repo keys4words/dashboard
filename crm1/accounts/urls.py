df49c662e37e6bb2adeb7dd4d8587de536bb515b
keys4words <keys4words@gmail.com>
2020-04-04 13:25:06 +0300

    add models to renders

--- crm1/accounts/urls.py
+++ crm1/accounts/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('products/', views.products),
-    path('customers/', views.customers),
-   
+    path('', views.home, name='home'),
+    path('products/', views.products, name='products'),
+    path('customers/<str:pk>/', views.customers, name='customer'),
 ]
